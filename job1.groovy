c7318725b6649e49f40de5e8e210a1eb0e90820b
igna-v <ivartabe1996@gmail.com>
2020-02-21 10:13:57 -0300

    fix job and shell script version 52

--- job1.groovy
+++ job1.groovy
@@ -1,9 +1,7 @@
 job('Git Master check file') {
 	description('Me fijo que esten los archivos')
 	scm{
-		git('https://github.com/igna-v/devOpsLab.git','master')
-		extensions {
-		}
+		git('https://github.com/igna-v/devOpsLab.git','master',{node -> node / 'extensions' << '' })
 	}
 	triggers {
 		scm('* * * * *')
